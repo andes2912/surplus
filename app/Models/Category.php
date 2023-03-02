90edca46d3cc880094220aac9bb67c449c4c1d12
Andri Desmana <andridesmana29@gmail.com>
2023-03-02 13:49:32 +0700

    Add api category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    protected $table = 'categories';
+    protected $guarded = [];
+
+    public function rawPayload($request)
+    {
+        $payload['name']    = $request->name ?? null;
+        $payload['enable']  = $request->enable ?? null;
+        return $payload;
+    }
 }
